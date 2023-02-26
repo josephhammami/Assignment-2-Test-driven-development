8acd7ae992d7958041538f4008cb77bad14ded2c
unknown <josephhammamii@gmail.com>
2023-02-26 21:58:10 +0100

    Added functionality to Player class

---
+++ src/dice/Player.py
@@ -0,0 +1,38 @@
+class Player:
+    """ 
+    A class representing a player in our dice game.
+    """
+
+    name = ""
+
+    def __init__(self, name):
+        """ 
+        Constructs a new Player object.
+        
+        Parameters:
+        - name (str): the initial name of the player.
+        """
+        self.name = name
+
+    def set_name(self,new_name):
+        """ 
+        sets the name of the player with the new given value.
+        
+        Parameters:
+        - new_name (str): the new name of the player.
+        """
+        self.name = new_name
+
+    def choose_name(self):
+        """
+        Allows the player to choose their name by entering it at the prompt.
+        """
+        name = input(">> ")
+        self.set_name(name)
+
+    def change_name(self):
+        """
+        Allows the user to change their name by entering a new name at the given prompt. 
+        """
+        new_name = input(">> ")
+        self.set_name(new_name)
