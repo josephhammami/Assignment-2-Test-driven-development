7ad4feaf50e1c5ca04269566910834f94cb520f2
Alloush95 <125370944+Alloush95@users.noreply.github.com>
2023-03-08 16:19:24 +0100

    Update DiceHand.py
    rättat!!

--- src/dice/DiceHand.py
+++ src/dice/DiceHand.py
@@ -16,7 +16,6 @@ class DiceHand:
         """
         self.player_score = 0
         self.dice = dice()
-        self.player_roll = self.dice.roll()
 
     def set_player_score(self, new_player_score):
         """
@@ -28,19 +27,21 @@ class DiceHand:
         """
         Rolls the dice for the given player and updates their score accordingly.
         """
-        
+        self.player_roll = self.dice.roll()
+
         if self.player_roll == 1:
             self.player_score = 0
             print(f"\nUhoh {player} rolled a {self.player_roll}! They lose all their points!")
-            return self.player_score
 
         else:
             self.player_score += self.player_roll
             print(f"{player} rolled a {self.player_roll}. Their total score is {self.player_score}!")
-            return self.player_score
 
     def hold(self, player):
         """
         Allows the given player to hold and keep their current score.
         """
         print(f"{player} is holding. Their current score is {self.player_score}.")
+        
+        
+        
