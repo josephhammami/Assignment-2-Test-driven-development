70071e6ff78db243da547f071ef4ce99d9fd361e
Alloush95 <125370944+Alloush95@users.noreply.github.com>
2023-03-08 16:23:22 +0100

    Update Player.py

--- src/dice/Player.py
+++ src/dice/Player.py
@@ -1,22 +1,23 @@
-""" 
+"""
 This class will hold the core functionalities of the player (user), playing our dice game.
 """
 
+
 class Player:
-    """ 
+    """
     A class representing a player in our dice game.
     """
 
     def __init__(self, name):
-        """ 
+        """
         Constructs a new Player object.
         """
         self.name = name
 
-    def set_name(self,new_name):
-        """ 
+    def set_name(self, new_name):
+        """
         Sets the name of the player with the new given value.
-        
+
         """
         self.name = new_name
 
@@ -35,8 +36,7 @@ class Player:
 
     def change_name(self):
         """
-        Allows the user to change their name by entering a new name at the given prompt. 
+        Allows the user to change their name by entering a new name at the given prompt.
         """
         new_name = input(">> ")
         self.set_name(new_name)
-        
