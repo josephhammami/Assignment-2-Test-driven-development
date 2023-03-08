1aca57f3b6b8ce076e9d0d13605f14a6a64a9a4b
Alloush95 <125370944+Alloush95@users.noreply.github.com>
2023-03-08 16:01:57 +0100

    Update test_dice_hand.py
    Den här testen är fixad

--- src/dice/tests/test_dice_hand.py
+++ src/dice/tests/test_dice_hand.py
@@ -1,28 +1,28 @@
 import unittest
-
 from DiceHand import DiceHand
 
 
 class TestDiceHand(unittest.TestCase):
-
     def setUp(self):
-        self.dice_hand = DiceHand()
+        """" Initializes a DiceHand object """
+        self.DiceHand = DiceHand()
 
-    def test_roll_dice(self):
-        # the dice roll always return a 5
-        self.dice_hand.player_roll = 5
-        diceRoll = self.dice_hand.roll_dice('Player 2 ')
-        self.assertEqual(diceRoll, 5)
-
-        # The dice roll return a 0
-        self.dice_hand.player_roll = 1
-        diceRoll = self.dice_hand.roll_dice('Player 3')
-        self.assertEqual(diceRoll, 0)
+    def test_rollDice(self):
+        """" The dice roll always return a 2 """
+        self.DiceHand.dice.roll = lambda: 2
+        self.DiceHand.roll_dice('Test')
+        self.assertEqual(self.DiceHand.player_score, 2)
+        
+        """" The dice roll always return a 0  """
+        self.DiceHand.dice.roll = lambda: 1
+        self.DiceHand.roll_dice('Test')
+        self.assertEqual(self.DiceHand.player_score, 0)
 
     def test_hold(self):
-        self.dice_hand.set_player_score(20)
-        self.dice_hand.hold("Player 1")
-        self.assertEqual(self.dice_hand.player_score, 20)
+        """" The player score is always 15 if player has hold """
+        self.DiceHand.set_player_score(15)
+        self.DiceHand.hold('Test')
+        self.assertEqual(self.DiceHand.player_score, 15)
 
 
 if __name__ == '__main__':
